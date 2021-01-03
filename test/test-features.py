4dd78021e557b54f6389653bbf40d19d38f89a3d
tpaviot <tpaviot@gmail.com>
2021-01-03 15:44:27 +0100

    More tests

--- test/test-features.py
+++ test/test-features.py
@@ -30,9 +30,41 @@ class TestFeatures(unittest.TestCase):
         pb = ps.SchedulingProblem('ProblemWithoutHorizon')
         self.assertIsInstance(pb, ps.SchedulingProblem)
 
-    def test_create_zero_length_(self) -> None:
-        task = ps.ZeroDurationTask('zt')
+    #
+    # Tasks
+    #
+    def test_create_task_zero_duration(self) -> None:
+        task = ps.ZeroDurationTask('zdt')
         self.assertIsInstance(task, ps.ZeroDurationTask)
+
+    def test_create_task_fixed_duration(self) -> None:
+        task = ps.FixedDurationTask('fdt', 1)
+        self.assertIsInstance(task, ps.FixedDurationTask)
+
+    def test_create_task_variable_duration(self) -> None:
+        task = ps.VariableDurationTask('vdt')
+        self.assertIsInstance(task, ps.VariableDurationTask)
+
+    #
+    # Workers
+    #
+    def test_create_worker(self) -> None:
+        worker = ps.Worker('wkr')
+        self.assertIsInstance(worker, ps.Worker)
+
+    def test_create_alternative_workers(self) -> None:
+        worker_1 = ps.Worker('wkr_1')
+        worker_2 = ps.Worker('wkr_2')
+        worker_3 = ps.Worker('wkr_3')
+        single_alternative_workers = ps.AlternativeWorkers([worker_1,
+                                                            worker_2], 1)
+        self.assertIsInstance(single_alternative_workers,
+                              ps.AlternativeWorkers)
+        double_alternative_workers = ps.AlternativeWorkers([worker_1,
+                                                            worker_2,
+                                                            worker_3], 2)
+        self.assertIsInstance(double_alternative_workers,
+                              ps.AlternativeWorkers)
 
     def test_eq_overloading(self) -> None:
         task_1 = ps.ZeroDurationTask('task1')
@@ -40,23 +72,74 @@ class TestFeatures(unittest.TestCase):
         self.assertEqual(task_1, task_1)
         self.assertNotEqual(task_1, task_2)
 
-    def test_dont_overlap_task_constraint(self) -> None:
-        # problem
-        pb1 = ps.SchedulingProblem("DontOverlapExample", horizon=10)
-        # tasks
-        t1 = ps.FixedDurationTask('t1', duration=2)
-        t2 = ps.FixedDurationTask('t2', duration=3)
-        t3 = ps.FixedDurationTask('t3', duration=4)
-        pb1.add_tasks([t1, t2,t3])
-        # constraints
-        c1 = ps.TaskStartAt(t2, 1)
-        c2 = ps.TasksDontOverlap(t2, t3)
-        pb1.add_constraints([c1, c2])
-        # solve
-        solver1 = ps.SchedulingSolver(pb1, verbosity=False)
-        solver1.solve()
-        # t3 should be scheduled last
-        self.assertEqual(t3.scheduled_start, 4)
+    #
+    # Single task constraints
+    #
+    def test_create_task_start_at(self) -> None:
+        task = ps.FixedDurationTask('task', 2)
+        c = ps.TaskStartAt(task, 1)
+        self.assertIsInstance(c, ps.TaskStartAt)
+        self.assertTrue(task.lower_bounded)
+        self.assertFalse(task.upper_bounded)
+        self.assertEqual(c.value, 1)
+
+    def test_create_task_start_after_strict(self) -> None:
+        task = ps.FixedDurationTask('task', 2)
+        c = ps.TaskStartAfterStrict(task, 3)
+        self.assertIsInstance(c, ps.TaskStartAfterStrict)
+        self.assertTrue(task.lower_bounded)
+        self.assertFalse(task.upper_bounded)
+        self.assertEqual(c.value, 3)
+
+    def test_create_task_start_after_lax(self) -> None:
+        task = ps.FixedDurationTask('task', 2)
+        c = ps.TaskStartAfterLax(task, 3)
+        self.assertIsInstance(c, ps.TaskStartAfterLax)
+        self.assertTrue(task.lower_bounded)
+        self.assertFalse(task.upper_bounded)
+        self.assertEqual(c.value, 3)
+
+    def test_create_task_end_at(self) -> None:
+        task = ps.FixedDurationTask('task', 2)
+        c = ps.TaskEndAt(task, 3)
+        self.assertIsInstance(c, ps.TaskEndAt)
+        self.assertFalse(task.lower_bounded)
+        self.assertTrue(task.upper_bounded)
+        self.assertEqual(c.value, 3)
+
+    def test_create_task_before_strict(self) -> None:
+        task = ps.FixedDurationTask('task', 2)
+        c = ps.TaskEndBeforeStrict(task, 3)
+        self.assertIsInstance(c, ps.TaskEndBeforeStrict)
+        self.assertFalse(task.lower_bounded)
+        self.assertTrue(task.upper_bounded)
+        self.assertEqual(c.value, 3)
+
+    def test_create_task_before_lax(self) -> None:
+        task = ps.FixedDurationTask('task', 2)
+        c = ps.TaskEndBeforeLax(task, 3)
+        self.assertIsInstance(c, ps.TaskEndBeforeLax)
+        self.assertFalse(task.lower_bounded)
+        self.assertTrue(task.upper_bounded)
+        self.assertEqual(c.value, 3)
+
+    #
+    # Two tasks constraints
+    #
+    def test_tasks_dont_overlap(self) -> None:
+        t_1 = ps.FixedDurationTask('t1', duration=2)
+        t_2 = ps.FixedDurationTask('t2', duration=3)
+        c_2 = ps.TasksDontOverlap(t_1, t_2)
+
+    def test_tasks_start_sync(self) -> None:
+        t_1 = ps.FixedDurationTask('t1', duration=2)
+        t_2 = ps.FixedDurationTask('t2', duration=3)
+        c_2 = ps.TasksStartSynced(t_1, t_2)
+
+    def test_tasks_end_sync(self) -> None:
+        t_1 = ps.FixedDurationTask('t1', duration=2)
+        t_2 = ps.FixedDurationTask('t2', duration=3)
+        c_2 = ps.TasksEndSynced(t_1, t_2)
 
 
 if __name__ == "__main__":
