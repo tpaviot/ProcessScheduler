94ca2d8dc75a5273fa14d2a4f5780e44e37e76be
tpaviot <tpaviot@gmail.com>
2021-01-04 10:52:43 +0100

    Add single task/single resource solver test

--- test/test-solver.py
+++ test/test-solver.py
@@ -23,18 +23,19 @@ import processscheduler as ps
 
 class TestSolver(unittest.TestCase):
     def test_schedule_single_fixed_duration_task(self) -> None:
-        pb = ps.SchedulingProblem('SingleTaskScehduling', horizon=2)
+        pb = ps.SchedulingProblem('SingleFixedDurationTaskScheduling', horizon=2)
         task = ps.FixedDurationTask('task', duration=2)
         pb.add_task(task)
         solver = ps.SchedulingSolver(pb)
-        solver.solve()
+        success = solver.solve()
+        self.assertTrue(success)
         # task should have been scheduled with start at 0
         # and end at 2
         self.assertEqual(task.scheduled_start, 0)
         self.assertEqual(task.scheduled_end, 2)
 
     def test_schedule_single_variable_duration_task(self) -> None:
-        pb = ps.SchedulingProblem('SingleTaskScehduling')
+        pb = ps.SchedulingProblem('SingleVariableDurationTaskScheduling')
         task = ps.VariableDurationTask('task')
         pb.add_task(task)
 
@@ -43,12 +44,35 @@ class TestSolver(unittest.TestCase):
         pb.add_constraint(ps.TaskEndAt(task, 4))
 
         solver = ps.SchedulingSolver(pb, verbosity=True)
-        solver.solve()
+        success = solver.solve()
+        self.assertTrue(success)
         # task should have been scheduled with start at 0
         # and end at 2
         self.assertEqual(task.scheduled_start, 1)
         self.assertEqual(task.scheduled_duration, 3)
         self.assertEqual(task.scheduled_end, 4)
+
+    def test_schedule_single_task_single_resource(self) -> None:
+        pb = ps.SchedulingProblem('SingleTaskSingleResource', horizon=7)
+
+        task = ps.FixedDurationTask('task', duration=7)
+        pb.add_task(task)
+
+        worker = ps.Worker('worker')
+        pb.add_resource(worker)
+
+        task.add_required_resource(worker)
+
+        solver = ps.SchedulingSolver(pb)
+        success = solver.solve()
+        self.assertTrue(success)
+
+        pb.print_solution()
+        # task should have been scheduled with start at 0
+        # and end at 2
+        self.assertEqual(task.scheduled_start, 0)
+        self.assertEqual(task.scheduled_end, 7)
+        self.assertEqual(task.assigned_resources, [worker])
 
 
 if __name__ == "__main__":
