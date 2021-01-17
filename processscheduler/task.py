3213223c0db07894fdf37d13cb4d8be3ddefb346
tpaviot <tpaviot@gmail.com>
2021-01-17 07:35:57 +0100

    Fix license headers

--- processscheduler/task.py
+++ processscheduler/task.py
@@ -1,16 +1,19 @@
-""" ProcessScheduler, Copyright 2020 Thomas Paviot (tpaviot@gmail.com)
+"""The module that defines the task related classes."""
 
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with
-this program. If not, see <http://www.gnu.org/licenses/>.
-"""
+# Copyright (c) 2020-2021 Thomas Paviot (tpaviot@gmail.com)
+#
+# This file is part of ProcessScheduler.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
 
 from typing import List, Optional
 
@@ -155,7 +158,7 @@ class UnavailabilityTask(FixedDurationTask):
     """ a task that tells that a resource is unavailable during this period. This
     task is not publicly exposed, it is used by the resource constraint ResourceUnavailability """
     def __init__(self, name: str, duration: int) -> None:
-        super().__init__(name, duration)
+        super().__init__(name, duration, work_amount=0, priority=0)
 
 class VariableDurationTask(Task):
     """ Tasj with a priori unknown duration. its duration is computed by the solver """
