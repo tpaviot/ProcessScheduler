5c2ab23080128d86a63bc8e0bd47e8781a27e60e
tpaviot <tpaviot@gmail.com>
2021-01-19 10:29:03 +0100

    Changed version to 0.1.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ CLASSIFIERS = [
 ]
 
 setup(name='ProcessScheduler',
-      version='0.1-dev',
+      version='0.1.0',
       description=DESCRIPTION,
       long_description=open("README.md").read(),
       long_description_content_type="text/markdown",
