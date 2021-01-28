404cf5b3c0cf073f1def02d81b0aee02f619024f
tpaviot <tpaviot@gmail.com>
2021-01-28 16:49:16 +0100

    Another fix to test_cumulative

--- test/test_cumulative.py
+++ test/test_cumulative.py
@@ -168,16 +168,19 @@ class TestCumulative(unittest.TestCase):
     def test_cumulative_cost(self):
         problem = ps.SchedulingProblem("CumulativeCost", horizon=5)
         t_1 = ps.FixedDurationTask('t1', duration=5)
-
+        t_2 = ps.FixedDurationTask('t2', duration=5)
+        t_3 = ps.FixedDurationTask('t3', duration=5)
         worker_1 = ps.CumulativeWorker('CumulWorker', size=3, cost_per_period=5)
         t_1.add_required_resource(worker_1)
+        t_2.add_required_resource(worker_1)
+        t_3.add_required_resource(worker_1)
 
         cost_ind = problem.add_indicator_resource_cost([worker_1])
 
         solution = ps.SchedulingSolver(problem).solve()
 
         self.assertTrue(solution)
-        self.assertEqual(solution.indicators[cost_ind.name], 15)
+        self.assertEqual(solution.indicators[cost_ind.name], 25)
         solution = ps.SchedulingSolver(problem).solve()
         self.assertTrue(solution)
 
