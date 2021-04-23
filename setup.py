72e67d9e050b18c1576c041e7fb0fe5fb73ba013
tpaviot <tpaviot@gmail.com>
2021-04-23 16:09:00 +0200

    Bumper version number to 0.6.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ CLASSIFIERS = [
 ]
 
 setup(name='ProcessScheduler',
-      version='0.5.0',
+      version='0.6.0',
       description=DESCRIPTION,
       long_description=open("README.md").read(),
       long_description_content_type="text/markdown",
