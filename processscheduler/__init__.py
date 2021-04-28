e2464854e78a4a11290ae6ee17198b7dbfced251
tpaviot <tpaviot@gmail.com>
2021-04-28 17:43:00 +0200

    Updates for release 0.6.1

--- processscheduler/__init__.py
+++ processscheduler/__init__.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 
-__VERSION__ = '0.6.0'
+__VERSION__ = '0.6.1'
 
 try:
     from z3 import BoolRef
