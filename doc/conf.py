7e3844fcf966ddf7310de0e14166a3178fe36f90
Dani Reinón <dani@dribo.es>
2021-07-09 12:48:16 +0200

    Updated version and added new features

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,7 @@ copyright = "2020-2021, Thomas Paviot"
 author = "Thomas Paviot"
 
 # The full version, including alpha/beta/rc tags
-version = "0.7.0"
+version = "0.7.1"
 release = version
 
 # -- RTD configuration ------------------------------------------------
