6a626f92be02a0c47211a0c7f97ad32337d58999
tpaviot <tpaviot@gmail.com>
2021-07-27 10:17:15 +0200

    Extend test_buffer

--- test/test_buffer.py
+++ test/test_buffer.py
@@ -32,7 +32,24 @@ class TestBuffer(unittest.TestCase):
             buffer = ps.Buffer("Buffer1", initial_state=10)
 
     def test_consume_buffer_1(self) -> None:
+        # only one buffer and one task
         pb = ps.SchedulingProblem("ConsumeBuffer1")
+
+        task_1 = ps.FixedDurationTask("task1", duration=3)
+        buffer = ps.Buffer("Buffer1", initial_state=10)
+
+        pb.add_constraint(ps.TaskStartAt(task_1, 5))
+        c1 = ps.TaskConsumeBuffer(task_1, buffer, quantity=3)
+        pb.add_constraint(c1)
+
+        solver = ps.SchedulingSolver(pb)
+        solution = solver.solve()
+        self.assertTrue(solution)
+        self.assertEqual(solution.buffers[buffer.name].state, [10, 7])
+        self.assertEqual(solution.buffers[buffer.name].state_change_times, [5])
+
+    def test_consume_buffer_2(self) -> None:
+        pb = ps.SchedulingProblem("ConsumeBuffer2")
 
         task_1 = ps.FixedDurationTask("task1", duration=3)
         task_2 = ps.FixedDurationTask("task2", duration=3)
@@ -58,7 +75,24 @@ class TestBuffer(unittest.TestCase):
         self.assertEqual(solution.buffers[buffer.name].state_change_times, [5, 10, 15])
 
     def test_feed_buffer_1(self) -> None:
+        # only one buffer and one task
         pb = ps.SchedulingProblem("FeedBuffer1")
+
+        task_1 = ps.FixedDurationTask("task1", duration=3)
+        buffer = ps.Buffer("Buffer1", initial_state=10)
+
+        pb.add_constraint(ps.TaskStartAt(task_1, 5))
+        c1 = ps.TaskFeedBuffer(task_1, buffer, quantity=3)
+        pb.add_constraint(c1)
+
+        solver = ps.SchedulingSolver(pb)
+        solution = solver.solve()
+        self.assertTrue(solution)
+        self.assertEqual(solution.buffers[buffer.name].state, [10, 13])
+        self.assertEqual(solution.buffers[buffer.name].state_change_times, [8])
+
+    def test_feed_buffer_2(self) -> None:
+        pb = ps.SchedulingProblem("FeedBuffer2")
 
         task_1 = ps.FixedDurationTask("task1", duration=3)
         task_2 = ps.FixedDurationTask("task2", duration=3)
@@ -82,6 +116,32 @@ class TestBuffer(unittest.TestCase):
         self.assertTrue(solution)
         self.assertEqual(solution.buffers[buffer.name].state, [10, 13, 15, 16])
         self.assertEqual(solution.buffers[buffer.name].state_change_times, [8, 13, 18])
+
+    def test_consume_feed_buffers_1(self) -> None:
+        # one task that consumes and feed two different buffers
+        pb = ps.SchedulingProblem("ConsumeFeedBuffer1")
+
+        task_1 = ps.FixedDurationTask("task1", duration=3)
+        buffer_1 = ps.Buffer("Buffer1", initial_state=10)
+        buffer_2 = ps.Buffer("Buffer2", initial_state=0)
+
+        pb.add_constraint(ps.TaskStartAt(task_1, 5))
+        c1 = ps.TaskConsumeBuffer(task_1, buffer_1, quantity=3)
+        pb.add_constraint(c1)
+
+        c2 = ps.TaskFeedBuffer(task_1, buffer_2, quantity=2)
+        pb.add_constraint(c2)
+
+        solver = ps.SchedulingSolver(pb)
+        solution = solver.solve()
+        self.assertTrue(solution)
+        self.assertEqual(solution.buffers[buffer_1.name].state, [10, 7])
+        self.assertEqual(solution.buffers[buffer_1.name].state_change_times, [5])
+        self.assertEqual(solution.buffers[buffer_2.name].state, [0, 2])
+        self.assertEqual(solution.buffers[buffer_2.name].state_change_times, [8])
+
+        # plot buffers
+        solution.render_gantt_matplotlib(show_plot=False)
 
 
 if __name__ == "__main__":
